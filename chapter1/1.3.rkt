d4993a8a523c601a0ba3634684e08a7804a0e283
lilde90 <leepan1990@gmail.com>
2015-05-16 08:45:22 +0800

    add solution for e1.3

---
+++ chapter1/1.3.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7)))
+
+(define (square x) (* x x))
+(define (sum-of-square x y) (+ (square x) (square y)))
+(define (max-two-square x y z) (cond ((> x y) (cond ((> y z) (sum-of-square x y))
+                                                    (else (sum-of-square x z))
+                                     )) ((<= x y) (cond ((> x z) (sum-of-square x y))
+                                                      (else (sum-of-square y z))
+                                                      ))))
+(max-two-square 1 3 4)
